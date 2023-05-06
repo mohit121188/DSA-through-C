7e114f0898a4eb6fc9d0e33b49c2712cb58bb667
Mohit Jain <130646657+mohit121188@users.noreply.github.com>
2023-05-06 16:49:11 +0530

    Update Program 2.c

--- QUEUE/Program 2.c
+++ QUEUE/Program 2.c
@@ -27,11 +27,9 @@ int main()
             printf("\nenter the element : ");
             scanf("%d",&x);
             enqueue(&q,x);
-            printf("\nRear = %d, Front = %d",q.rear,q.front);
             break;
         case 2:
-            printf("\nremoved element is : %d",dequeue(&q));
-            printf("\nRear= %d, Front= %d",q.rear,q.front);
+            printf("\nremoved element is : %d\n",dequeue(&q));
             break;
         case 3:
             printf("\nQuittint the Application");
@@ -51,47 +49,44 @@ void enqueue(cqueue *pq,int x)
         printf("\n!!!Circular Queue Overflow!!!\n");
         return;
     }
-    if(pq->rear==-1&&pq->front==-1)
-    {
+    if(pq->rear==-1)
         pq->rear=pq->front=0;
-    }
     else
-        if(pq->rear==max-1&&pq->front!=0)
-    {
+    if(pq->rear==max-1)
         pq->rear=0;
-    }
     else
-    {
-        pq->rear=pq->rear+1;
-    }
+        pq->rear++;
     pq->arr[pq->rear]=x;
 }
 int dequeue(cqueue *pq)
 {
     int x;
-    if(pq->rear==-1&&pq->front==-1)
+    if(pq->front==-1)
     {
-        printf("!!!Circular Queue Underflow!!!\n");
+        printf("\n!!!Circular Queue Underflow!!!");
         return -1;
     }
     x=pq->arr[pq->front];
     if(pq->front==pq->rear)
-    {
         pq->front=pq->rear=-1;
-    }
     else
-        if(pq->front==max-1&&pq->rear!=max-1)
-    {
+    if(pq->front==max-1)
         pq->front=0;
-    }
     else
-    {
-        pq->front=pq->front+1;
-    }
+        pq->front++;
     return x;
 }
 
-/*sample output : 
+/* Sample Output
+
+select the operation :
+1.enqueue
+2.dequeue
+3.quit
+enter your choice : 2
+
+!!!Circular Queue Underflow!!!
+removed element is : -1
 
 select the operation :
 1.enqueue
@@ -100,7 +95,6 @@ select the operation :
 enter your choice : 1
 
 enter the element : 10
-Rear = 0, Front = 0
 
 select the operation :
 1.enqueue
@@ -109,7 +103,6 @@ select the operation :
 enter your choice : 1
 
 enter the element : 20
-Rear = 1, Front = 0
 
 select the operation :
 1.enqueue
@@ -118,7 +111,6 @@ select the operation :
 enter your choice : 1
 
 enter the element : 30
-Rear = 2, Front = 0
 
 select the operation :
 1.enqueue
@@ -127,7 +119,6 @@ select the operation :
 enter your choice : 1
 
 enter the element : 40
-Rear = 3, Front = 0
 
 select the operation :
 1.enqueue
@@ -136,7 +127,6 @@ select the operation :
 enter your choice : 1
 
 enter the element : 50
-Rear = 4, Front = 0
 
 select the operation :
 1.enqueue
@@ -147,7 +137,6 @@ enter your choice : 1
 enter the element : 60
 
 !!!Circular Queue Overflow!!!
-Rear = 4, Front = 0
 
 select the operation :
 1.enqueue
@@ -156,7 +145,6 @@ select the operation :
 enter your choice : 2
 
 removed element is : 10
-Rear= 4, Front= 1
 
 select the operation :
 1.enqueue
@@ -165,7 +153,6 @@ select the operation :
 enter your choice : 2
 
 removed element is : 20
-Rear= 4, Front= 2
 
 select the operation :
 1.enqueue
@@ -174,7 +161,6 @@ select the operation :
 enter your choice : 1
 
 enter the element : 60
-Rear = 0, Front = 2
 
 select the operation :
 1.enqueue
@@ -183,7 +169,6 @@ select the operation :
 enter your choice : 1
 
 enter the element : 70
-Rear = 1, Front = 2
 
 select the operation :
 1.enqueue
@@ -194,7 +179,6 @@ enter your choice : 1
 enter the element : 80
 
 !!!Circular Queue Overflow!!!
-Rear = 1, Front = 2
 
 select the operation :
 1.enqueue
@@ -203,7 +187,6 @@ select the operation :
 enter your choice : 2
 
 removed element is : 30
-Rear= 1, Front= 3
 
 select the operation :
 1.enqueue
@@ -212,7 +195,6 @@ select the operation :
 enter your choice : 2
 
 removed element is : 40
-Rear= 1, Front= 4
 
 select the operation :
 1.enqueue
@@ -221,7 +203,6 @@ select the operation :
 enter your choice : 2
 
 removed element is : 50
-Rear= 1, Front= 0
 
 select the operation :
 1.enqueue
@@ -230,7 +211,6 @@ select the operation :
 enter your choice : 2
 
 removed element is : 60
-Rear= 1, Front= 1
 
 select the operation :
 1.enqueue
@@ -239,17 +219,15 @@ select the operation :
 enter your choice : 2
 
 removed element is : 70
-Rear= -1, Front= -1
 
 select the operation :
 1.enqueue
 2.dequeue
 3.quit
 enter your choice : 2
-!!!Circular Queue Underflow!!!
 
+!!!Circular Queue Underflow!!!
 removed element is : -1
-Rear= -1, Front= -1
 
 select the operation :
 1.enqueue
@@ -258,6 +236,5 @@ select the operation :
 enter your choice : 3
 
 Quittint the Application
-Process returned 0 (0x0)   execution time : 71.736 s
-Press any key to continue.
-*/
+Process returned 0 (0x0)   execution time : 66.744 s
+Press any key to continue. */
