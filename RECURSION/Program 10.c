da19bab9ae0b95853d7e2ea0ee8bfab0ab33ba73
Mohit Jain <130646657+mohit121188@users.noreply.github.com>
2023-07-16 07:31:53 +0530

    Update Program 10.c

--- RECURSION/Program 10.c
+++ RECURSION/Program 10.c
@@ -1,19 +1,78 @@
+//style 1:
 #include<stdio.h>
-int greatest(int arr[],int size)
+int greatest(int arr[],int n)
 {
         static int max=0,index=0;
-        if(index==5)
+        if(index==n)
             return max;
         if(max<arr[index])
             max=arr[index];
         index++;
-        greatest(arr,size);
+        greatest(arr,n);
 }
 int main()
 {
-    int arr[5]={30,1000,40,50,10};
-    int max;
-    max=greatest(arr,5);
+    int *arr=NULL;
+    int n,i,max;
+    printf("\nenter the number of elements to be stored : ");
+    scanf("%d",&n);
+    arr=(int *)malloc(n*sizeof(int));
+    if(arr==NULL)
+    {
+        printf("\nError:Memory allocation problem");
+        return 1;
+    }
+    for(i=0;i<=n-1;i++)
+    {
+        printf("\nenter the %dth element in the array : ",i);
+        scanf("%d",(arr+i));
+    }
+    max=greatest(arr,n);
     printf("\ngreatest number is %d",max);
     return 0;
 }
+
+
+//style 2:
+#include<stdio.h>
+#include<stdlib.h>
+int max(int,int);
+int findMax(int [],int);
+int main()
+{
+    int n;
+    int *arr=NULL;
+    int i;
+    printf("\nenter the number of elements to be stored in the array : ");
+    scanf("%d",&n);
+    arr=(int *)malloc(n*sizeof(int));
+    if(arr==NULL)
+    {
+        printf("\nERROR:memory allocation problem");
+        return 1;
+    }
+    for(i=0;i<=n-1;i++)
+    {
+        printf("\nenter %dth element in the array : ",i);
+        scanf("%d",(arr+i));
+    }
+    printf("\nmax element is %d",findMax(arr,n));
+    return 0;
+}
+
+int max(int a,int b)
+{
+    if(a>=b)
+        return a;
+    else
+        return b;
+}
+
+int findMax(int arr[],int n)
+{
+    if(n==1)
+        return(arr[n-1]);
+    else
+        return(max(arr[n],findMax(arr,n-1)));
+}
+
